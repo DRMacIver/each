ee93c942b21d767db263cc5476986482ac91630a
David R. MacIver <david@drmaciver.com>
2018-08-11 16:36:04 +0100

    formatting

--- src/each/__init__.py
+++ src/each/__init__.py
@@ -1,4 +1,4 @@
-from each.each import Each
+from each.each import SHELL, Each
 from each.version import __version__, __version_info__
 
-__all__ = ["Each", "__version__", "__version_info__"]
+__all__ = ["Each", "__version__", "__version_info__", "SHELL"]
