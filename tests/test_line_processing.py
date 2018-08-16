c5d50422464efa7433b41a2e3815300c8d042c1f
Jonathan Lange <jml@mumak.net>
2018-08-16 12:51:00 +0100

    In-process tests for line processing

--- tests/test_line_processing.py
+++ tests/test_line_processing.py
@@ -1,6 +1,51 @@
 import os
 
+import pytest
+
+from each import Each
 from each.each import LineWorkItem
+from test_main import get_directory_contents
+
+
+@pytest.mark.parametrize("processes", [1, 2, 4])
+@pytest.mark.parametrize("stderr", [False, True])
+@pytest.mark.parametrize("stdin", [False, True])
+def test_processes_each_line(tmpdir, processes, stderr, stdin):
+    input_path = tmpdir.join("input")
+    output_path = tmpdir.mkdir("output")
+    with input_path.open("w") as input_file:
+        for i in range(10):
+            input_file.write("hello %d\n" % (i,))
+
+    if stdin:
+        if stderr:
+            command = "cat >&2"
+        else:
+            command = "cat"
+    else:
+        if stderr:
+            command = "echo {} | cat >&2"
+        else:
+            command = "echo {}"
+
+    each = Each(
+        command=command,
+        source=input_path,
+        destination=output_path,
+        processes=processes,
+        stdin=stdin,
+    )
+    each.clear_queue()
+
+    output_files = sorted(output_path.listdir())
+    assert output_files == [output_path.join("hello %d" % (i,)) for i in range(10)]
+
+    for i, f in enumerate(output_files):
+        line = "hello %d" % (i,)
+        expected = {"status": "0", "in": line, "out": line, "err": ""}
+        if stderr:
+            expected.update({"out": "", "err": line})
+        assert get_directory_contents(f) == expected
 
 
 def test_creates_pipe():
